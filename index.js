90cbc4dec2797f316ebb1da428a935561a92a73b
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-04-04 22:59:21 +0530

    Learning html

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// function for meter tag.
+function increaseMeter(){
+    var i = document.getElementById("mtr");
+    i.value+=10;
+}
+
+function decreaseMeter(){
+    var i = document.getElementById("mtr");
+    i.value-=10;
+}
+
+
+// function for progress tag
+
+function incr(){
+    var x = document.getElementById("file-progress");
+    x.value+=10;
+}
+function decr(){
+    var x = document.getElementById("file-progress");
+    x.value-=10;
+}
+
+
